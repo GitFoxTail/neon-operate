31b180b83bbe0f874e7858ae3c2b9063e53629e3
Yuji <heiseirider20@yahoo.co.jp>
2025-02-10 22:05:59 +0900

    first commit

---
+++ src/components/camera_capture.tsx
@@ -0,0 +1,45 @@
+import { useState, type ChangeEvent } from "react";
+
+interface CameraCaptureProps {
+    onImageCapture?: (imageUrl: string) => void;
+}
+
+const CameraCapture: React.FC<CameraCaptureProps> = ({onImageCapture}) => {
+    const [ imageUrl, setImageUrl ] = useState<string | null>(null);
+
+    const handleCapture = (e: ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if(file){
+            const url = URL.createObjectURL(file);
+            setImageUrl(url);
+            onImageCapture?.(url);
+        }
+    }
+
+    return (
+        <div>
+            <div>
+                <label>
+                    <span>写真を撮影</span>
+                    <input 
+                        type="file"
+                        accept="image/*"
+                        capture="environment"
+                        onChange={handleCapture}
+                        className="hidden"    
+                    />
+                </label>
+            </div>
+
+            {imageUrl && (
+                <div>
+                    <img
+                        src={imageUrl}
+                        alt="撮影した写真"                    ></img>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default CameraCapture;
